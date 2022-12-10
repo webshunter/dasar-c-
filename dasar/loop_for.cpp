3089d3234333db7f67987b62c489a750ba41a53a
webshunternet <webshunternet@gmail.com>
2022-12-10 10:36:00 +0700

    cpp test

---
+++ dasar/loop_for.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    for (int ulang = 0; ulang < 10; ulang++)
+    {
+        std::cout << ulang << " - " << ulang * 10 << "\n";
+    }
+    return 0;
+}
